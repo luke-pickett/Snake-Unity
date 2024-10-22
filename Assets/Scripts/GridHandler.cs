12c2e342bc5afffd6693a1dd9ab04c856edb4319
Luke Pickett <105627015+luke-pickett@users.noreply.github.com>
2024-10-22 15:16:54 -0600

    Added Coordinates and method for grabbing gameobejct based on them

--- Assets/Scripts/GridHandler.cs
+++ Assets/Scripts/GridHandler.cs
@@ -1,24 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class GridGeneration : MonoBehaviour
+public class GridHandler : MonoBehaviour
 {
+    public static GridHandler instance;
     [SerializeField] private GameObject tilePrefab;
-    [SerializeField] private GameObject gridParent;
-    public int gridSizeX;
-    public int gridSizeY;
+    [SerializeField] private int gridSizeX;
+    [SerializeField] private int gridSizeY;
     private float _spaceBetweenTiles;
+    private GameObject[,] _grid;
 
     private void Awake()
     {
+        instance = this;
         _spaceBetweenTiles = tilePrefab.transform.localScale.x;
-    }
-
-    private void Start()
-    {
+        _grid = new GameObject[gridSizeX, gridSizeY];
+        
+        GameObject gridParent = new GameObject();
+        gridParent.name = "Grid";
+        
         Vector3 pos = new Vector3(0, 0, 0);
         Vector3 displacementX = new Vector3(_spaceBetweenTiles, 0, 0);
         Vector3 displacementY = new Vector3(0, 0, _spaceBetweenTiles);
@@ -28,9 +32,25 @@ public class GridGeneration : MonoBehaviour
             for (int j = 0; j < gridSizeY; j++)
             {
                 GameObject tile = Instantiate(tilePrefab, pos, new Quaternion(), gridParent.transform);
+                tile.name = $"({i},{j})";
+                _grid[i, j] = tile;
                 pos += displacementY;
             }
             pos += displacementX;
         }
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    public GameObject GrabTile(int xValue, int yValue)
+    {
+        if (xValue > gridSizeX || yValue > gridSizeY)
+        {
+            return null;
+        }
+        return _grid[xValue, yValue];
     }
 }
