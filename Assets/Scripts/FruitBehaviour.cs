e8f44692d4c6bd94befd05111563ee0d3d406492
ShaiYosef <oliveryosef61@gmail.com>
2024-10-23 12:13:25 -0600

    Added audio and visual folders to assets

---
+++ Assets/Scripts/FruitBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitBehaviour : MonoBehaviour
+{
+    private GameObject _fruit;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
