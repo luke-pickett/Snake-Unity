a0099b6489d9beae644545fb224e994e2642320a
Luke Pickett <105627015+luke-pickett@users.noreply.github.com>
2024-10-22 19:52:12 -0600

    Added method to find surrounding tiles

--- Assets/Scripts/GridHandler.cs
+++ Assets/Scripts/GridHandler.cs
@@ -33,6 +33,9 @@ public class GridHandler : MonoBehaviour
             {
                 GameObject tile = Instantiate(tilePrefab, pos, new Quaternion(), gridParent.transform);
                 tile.name = $"({i},{j})";
+                TileProperties properties = tile.AddComponent<TileProperties>();
+                properties.xValue = i;
+                properties.yValue = j;
                 _grid[i, j] = tile;
                 pos += displacementY;
             }
@@ -65,5 +68,57 @@ public class GridHandler : MonoBehaviour
         int col = gridSizeY - 1;
         
         return _grid[(int) rows / 2, (int) col / 2];
+    }
+    
+    // Returns an array of tiles to north, east, south, and west, in that order
+    // if the tile is on the edge it will fill the tile that does not exist with null
+    public GameObject[] GrabAdjacentTiles(int xValue, int yValue)
+    {
+        GameObject[] returnArray = new GameObject[4];
+        // Grabs North tile
+        if (yValue < gridSizeY-1)
+        {
+            returnArray[0] = GrabTile(xValue, yValue + 1);
+        }
+        else
+        {
+            returnArray[0] = null;
+        }
+        
+        // Grabs East tile
+        if (xValue < gridSizeX - 1)
+        {
+            returnArray[1] = GrabTile(xValue + 1, yValue);
+        }
+        else
+        {
+            returnArray[1] = null;
+        }
+        
+        // Grabs South tile
+        if (yValue > 0)
+        {
+            returnArray[2] = GrabTile(xValue, yValue-1);
+        }
+        else
+        {
+            returnArray[2] = null;
+        }
+        
+        // Grabs West tile
+        if (xValue > 0)
+        {
+            returnArray[3] = GrabTile(xValue - 1, yValue);
+        }
+        else
+        {
+            returnArray[3] = null;
+        }
+        return returnArray;
+    }
+
+    private void Start()
+    {
+        
     }
 }
---
+++ Assets/Scripts/TileProperties.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileProperties : MonoBehaviour
+{
+    [HideInInspector] public int xValue;
+    [HideInInspector] public int yValue;
+}
