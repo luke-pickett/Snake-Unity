f94d24ad5897b082331f0ecfab02f8ac488ead5f
Luke Pickett <105627015+luke-pickett@users.noreply.github.com>
2024-10-23 11:39:00 -0600

    Added new tile properties for contained objects and added a gameloop script

--- Assets/Scripts/TileProperties.cs
+++ Assets/Scripts/TileProperties.cs
@@ -6,4 +6,5 @@ public class TileProperties : MonoBehaviour
 {
     [HideInInspector] public int xValue;
     [HideInInspector] public int yValue;
+    public List<GameObject> contains = new List<GameObject>();
 }
