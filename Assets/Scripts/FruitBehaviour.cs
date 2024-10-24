dfeb5d8d64fc741ef0b74a2cea2de32dd58adb53
ShaiYosef <oliveryosef61@gmail.com>
2024-10-23 19:19:57 -0600

    Updated Fruit Spawning

--- Assets/Scripts/FruitBehaviour.cs
+++ Assets/Scripts/FruitBehaviour.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class FruitBehaviour : MonoBehaviour
 {
-    [SerializeField] private GameObject _fruit;
+    [SerializeField] private GameObject[] _fruitGallery; // Array of fruit meshes
     [SerializeField] private float _spawnTimer;
-    private float _timer;
-
-    void Start()
-    {
-        
-    }
-
+    private float _timer = 0;
 
     void Update()
     {
         _timer += Time.deltaTime;
+
         if (_timer > _spawnTimer)
         {
             _timer = 0;
-            GridHandler.instance.PlaceObject(_fruit, 0, 0);
+            SpawnFruit();
         }
+    }
+
+    void SpawnFruit()
+    {
+        // Get random position in grid
+        int randomX = Random.Range(0, GridHandler.instance.gridSizeX);
+        int randomY = Random.Range(0, GridHandler.instance.gridSizeY);
+
+        // Select random fruit from the gallery
+        GameObject randomFruit = _fruitGallery[Random.Range(0, _fruitGallery.Length)];
+
+        // Place the fruit at the random grid position
+        GridHandler.instance.PlaceObject(randomFruit, randomX, randomY);
     }
 }
